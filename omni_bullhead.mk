bc1ff6d30f0e2b5532a346d74e8d9c6f5d71f4fb
Unknown <SyberHexen@gmail.com>
2018-02-19 19:45:38 -0800

    Switch branding
    change Lineage to omni

--- omni_bullhead.mk
+++ omni_bullhead.mk
@@ -1,18 +1,21 @@
 # Inherit omni-specific board config
 include device/lge/bullhead/BoardConfigOmni.mk
 
-# Inherit base AOSP device configuration
-$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Override product naming for Omni
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
-PRODUCT_RESTRICT_VENDOR_FILES := false
+TARGET_MANUFACTURER := LGE
+#PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:8.0.0/OPR4.170623.009/4302492:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 8.0.0 OPR4.170623.009 4302492 release-keys"
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:8.1.0/OPM3.171019.014/4503998:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM3.171019.014 4503998 release-keys"
