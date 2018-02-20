4426ef5ed3bda28eb4a5837dbd79484083faf2d3
Unknown <SyberHexen@gmail.com>
2018-02-19 21:06:14 -0800

    Fix dex-preoptimization
    *Change AICP_BUILDTYPE),UNOFFICIAL) to  TARGET_BUILD_VARIANT),eng)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,7 +102,7 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
-  ifneq ($(AICP_BUILDTYPE),UNOFFICIAL)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
