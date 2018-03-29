d356e9a5069a51398af06b93793cded0d726b62a
SyberHexen <SyberHexen@gmail.com>
2018-03-29 15:05:45 -0700

    Update mk files

--- aosp_bullhead.mk
+++ aosp_bullhead.mk
@@ -31,10 +31,11 @@ PRODUCT_MODEL := AOSP on BullHead
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-PRODUCT_COPY_FILES += device/lge/bullhead/fstab.aosp_bullhead:root/fstab.bullhead
+#PRODUCT_COPY_FILES += device/lge/bullhead/fstab.aosp_bullhead:root/fstab.bullhead
 
 $(call inherit-product, device/lge/bullhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/bullhead/bullhead-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/bullhead/device-vendor.mk)
 
 PRODUCT_PACKAGES += \
     Launcher3 \
--- omni_bullhead.mk
+++ omni_bullhead.mk
@@ -1,11 +1,11 @@
 # Inherit omni-specific board config
-include device/lge/bullhead/BoardConfigOmni.mk
+#include device/lge/bullhead/BoardConfigOmni.mk
+
+# Inherit some common OmniRom stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
-
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
 
 
 ## Device identifier. This must come after all inclusions
@@ -13,9 +13,8 @@ PRODUCT_NAME := omni_bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
-#PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:8.1.0/OPM3.171019.016/4565142:user/release-keys" \
+    BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM3.171019.016/4565142:user/release-keys \
     PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM3.171019.016 4565142 release-keys"
