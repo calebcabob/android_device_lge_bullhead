d7b05c5150d000b5f13f1f48fbf69b838a2cfa20
SyberHexen <SyberHexen@gmail.com>
2018-03-29 15:03:42 -0700

    Remove use of BoardConfigOmni & Update BoardConfig
    * Inline kernel (omnify)
    * Remove CPUSETS
    * SnapdragonCamera2 support
    * Update DEXPREOPT
    * Wait for qseecom
    * Remove USE_CLANG (It's default)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,9 +26,20 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 
-ENABLE_CPUSETS := true
-
 TARGET_NO_BOOTLOADER := true
+
+#ENABLE_CPUSETS := true
+
+# Inline kernel
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_SOURCE := kernel/lge/bullhead
+TARGET_KERNEL_CONFIG := omni_bullhead_defconfig
+
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
+TARGET_COMPILE_WITH_MSM_KERNEL := true
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
@@ -40,6 +51,14 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 msm_poweroff.download_mode=0
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
+
+# SnapdragonCamera2
+BOARD_USES_SNAPDRAGONCAMERA_VERSION := 2
+
+# Ignore DSPManager's audio_effects config file (prevents file creation in system/vendor)
+TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
 
 BOARD_USES_ALSA_AUDIO := true
 AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
@@ -104,6 +123,7 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
       WITH_DEXPREOPT := true
     endif
   endif
@@ -133,6 +153,10 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 
+# Keymaster - Wait for qseecom to load
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# SElinux
 BOARD_SEPOLICY_DIRS += \
     device/lge/bullhead/sepolicy
 
@@ -153,10 +177,6 @@ NXP_CHIP_TYPE := 2
 
 #Enable peripheral manager
 TARGET_PER_MGR_ENABLED := true
-
-USE_CLANG_PLATFORM_BUILD := true
-
-USE_CLANG_PLATFORM_BUILD := true
 
 TARGET_FS_CONFIG_GEN += device/lge/bullhead/config.fs
 
--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -1,15 +1,14 @@
 # Inline kernel
-TARGET_NO_KERNEL := false
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
-TARGET_KERNEL_CONFIG := bullhead_defconfig
+TARGET_KERNEL_CONFIG := omni_bullhead_defconfig
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
-# Ignore DSPManager's audio_effects config file
+# Ignore DSPManager's audio_effects config file (prevents file in system/vendor)
 TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
 
 # Enable vendor image symlink
@@ -18,15 +17,13 @@ BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
 # SnapdragonCamera2
 BOARD_USES_SNAPDRAGONCAMERA_VERSION := 2
 
+# SELinux
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.build.selinux=1
+
 # Keymaster - Wait for qseecom to load
 #TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
-# TWRP
-#TW_THEME := portrait_hdpi
-#RECOVERY_GRAPHICS_USE_LINELENGTH := true
-#BOARD_SUPPRESS_SECURE_ERASE := true
-#RECOVERY_SDCARD_ON_DATA := true
-#BOARD_HAS_NO_REAL_SDCARD := true
-#TW_INCLUDE_CRYPTO := true
-#TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-#TARGET_RECOVERY_QCOM_RTC_FIX := true
+
+
+
